27c79372f59cd571f95ff628321ec54407b6b039
ascotlan <antonio.k.scotland@gmail.com>
2023-10-30 18:30:01 +0000

    implement the necessary route, controller, and view code for an administrators to see a list of all categories and create new categories.

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,10 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
+  namespace :admin do
+    resources :categories, only: [:index, :new, :create]
+  end
+
   get 'about', to: 'about#index'  # New route for the About page
 
   # The priority is based upon order of creation: first created -> highest priority.
